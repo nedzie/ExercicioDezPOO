825572e004822eedde756618df1e2f7f0525ac2b
nedzie <marcos-elivelton@hotmail.com>
2022-03-08 16:39:36 -0300

    Add project files.

---
+++ ExercicioDez.ConsoleApp/Aluno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioDez.ConsoleApp
+{
+    internal class Aluno
+    {
+        public double[] notasProvas = new double[2];
+        public double[] pesoProvas = new double[2];
+        public double mediaPonderada;
+
+        public double ObterMediaPonderada(double[] notasProvas, double[] pesoProvas)
+        {
+            double somaNotasProvas = 0;
+            double somaPesos = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                somaNotasProvas += notasProvas[i] * pesoProvas[i];
+                somaPesos += pesoProvas[i];
+            }
+            mediaPonderada = somaNotasProvas / somaPesos;
+            return mediaPonderada;
+        }
+    }
+}
---
+++ ExercicioDez.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExercicioDez.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno marcos = new Aluno();
+            Console.Write("Informe a nota da 1ª prova: \n> ");
+            marcos.notasProvas[0] = double.Parse(Console.ReadLine());
+            Console.Write("Informe o peso da 1ª prova: \n> ");
+            marcos.pesoProvas[0] = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe a nota da 2ª prova: \n> ");
+            marcos.notasProvas[1] = double.Parse(Console.ReadLine());
+            Console.Write("Informe o peso da 2ª prova: \n> ");
+            marcos.pesoProvas[1] = double.Parse(Console.ReadLine());
+
+            marcos.mediaPonderada = marcos.ObterMediaPonderada(marcos.notasProvas, marcos.pesoProvas);
+
+            Console.WriteLine("A média ponderada destas notas é de: " + marcos.mediaPonderada.ToString("#.##") + ".");
+        }
+    }
+}
